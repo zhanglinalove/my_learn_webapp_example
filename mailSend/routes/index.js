29c823dd74d38e831a495d0928c77ec736e43acf
zhanglinalove <zhanglinalove@github.com>
2018-05-07 22:54:01 +0800

    update file

--- mailSend/routes/index.js
+++ mailSend/routes/index.js
@@ -8,29 +8,22 @@ router.get('/', function(req, res, next) {
 
 // 补充 author 代码
 
-/* function author(){
-  const to = who;
-  const subject = `测试nodeMailer邮件发送功能`;
-  const html =
-  `<p>HI ，亲爱高阳老师 </p>` +
-  `<p> 我正在测试 nodeMailer 发送邮件的功能,收到邮件请不要惊讶，哈哈哈哈！！！</p>`;
-}
- */
 
 const author = {
   host: 'smtp.163.com', //用的126邮箱
   port: 465,
   secure: true, // upgrade later with STARTTLS
   auth: {
-      user: 'zhanglina19880929@163.com',  //使用自己的邮箱名；
-      pass: '137965277206@ZLN'  //使用自己的密码；
+      user: 'XX@163.com',  //使用自己的邮箱名；
+      pass: 'password'  //使用自己的密码；
     }
 };
 
 const transporter = mailer.createTransport(smtpTransport(author)); 
 // 补充mailOptions代码。
 const mailOptions = {
-     to : '徐老师',
+     from:'XX@163.com',
+     to : 'XX@163.com',
      subject :'测试nodeMailer邮件发送功能',
      html :'<p>HI ，亲爱高阳老师 </p>' +
     '<p> 我正在测试 nodeMailer 发送邮件的功能,收到邮件请不要惊讶，哈哈哈哈！！！</p>'
